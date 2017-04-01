dfaf0450521c76ecfdb7d375936225ce5cf8826a
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2017-04-01 17:51:43 -0400

    verify script

---
+++ laser/simp/run_and_verify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#./maplesat  -ccmin-mode=0 -never-gc -always-restart -lsr-num -lsr-out=lsr $1 | tee genlsr
+#./maplesat  -ccmin-mode=0 -never-gc -always-restart -lsr-cert-cls-out=cert -lsr-in=lsr $1 | tee gencert
+#./maplesat  -ccmin-mode=0 -never-gc -always-restart -lsr-cert-cls-in=cert -lsr-in=lsr $1 | tee checkcert
+
+./maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=0 -always-restart -conf-side-lsr -verb=0 $1
+./maplesat -lsr-in=lsr  -lsr-cert-cls-out=clauses -lsr-num -ccmin-mode=0 -always-restart -conf-side-lsr -verb=0 $1
+./maplesat -lsr-in=lsr  -lsr-cert-cls-in=clauses -never-gc -lsr-num -ccmin-mode=0 -always-restart -conf-side-lsr -verb=0 $1
