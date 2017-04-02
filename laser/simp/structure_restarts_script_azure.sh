725d5cbbce86c5fd93fba8b706c0fcb5d101b117
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2017-04-02 01:29:34 -0400

    script

--- laser/simp/structure_restarts_script_azure.sh
+++ laser/simp/structure_restarts_script_azure.sh
@@ -19,12 +19,12 @@ fi
 echo "-------------"
 echo "Never restart"
 echo "-------------"
-timeout 600 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr -never-restart -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf  $bb
+timeout 300 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr -never-restart -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf  $bb
 echo "--------------"
 echo "Always restart"
 echo "--------------"
-timeout 600 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr -always-restart -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf $bb
+timeout 300 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr -always-restart -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf $bb
 echo "-----------------"
 echo "Standard restarts"
 echo "-----------------"
-timeout 600 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr  -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf $bb
+timeout 300 $maplesat -lsr-out=lsr -all-dec-out=alldecs -lsr-num -ccmin-mode=2 -verb=0  -conf-side-lsr -avg-clause-lsr  -cmty-file=${base_dir}/cmty/${name}.cmty ${base_dir}/cnf/${name}.cnf $bb
